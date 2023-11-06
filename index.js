8a3c426444baf74bbf6ee24ae697b58cc67148f5
sattamap <sattamap@gmail.com>
2023-11-07 02:58:15 +0600

    Implemented API endpoint for updating a job by ID

--- index.js
+++ index.js
@@ -20,98 +20,126 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const jobsCollection = client.db('JobNestDB').collection('jobs');
-    const appliedJobsCollection = client.db('JobNestDB').collection('appliedJobs');
+        const jobsCollection = client.db('JobNestDB').collection('jobs');
+        const appliedJobsCollection = client.db('JobNestDB').collection('appliedJobs');
 
-          //job related API
-          app.get('/jobs', async (req, res) => {
+        //API endpoint for retrieving all jobs
+        app.get('/jobs', async (req, res) => {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
-            })
+        })
 
-       // API endpoint for retrieving a single job by ID
+        // API endpoint for retrieving a single job by ID
 
-            app.get('/jobs/:id',async(req,res)=>{
-                const id = req.params.id;
-                const query = {_id: new ObjectId(id)}
-            
-                const result = await jobsCollection.findOne(query);
-                res.send(result);
-            })
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
 
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
 
-
-       
+       //API for job  creating a new job
 
         app.post('/jobs', async (req, res) => {
             const newJob = req.body;
             newJob.jobApplicantsNumber = 0; // Initialize the applicants field
             const result = await jobsCollection.insertOne(newJob);
             res.send(result);
-          });
+        });
 
 
         //   app.post('/appliedJobs', async (req, res) => {
         //         const applicationData = req.body;
         //         console.log(applicationData);
         //         const result = await appliedJobsCollection.insertOne(applicationData);
-        
+
         //         res.send(result);
-        
+
         //     })
 
+           //job applicant count and inserted application data in /appliedJobs API
 
         app.post('/appliedJobs', async (req, res) => {
             const applicationData = req.body;
-            
+
             // Update the job's applicant count using the $inc operator
             const jobId = applicationData.jobId;
-             // Convert the jobId to ObjectId
-  const jobObjectId = new ObjectId(jobId);
-          
+            // Convert the jobId to ObjectId
+            const jobObjectId = new ObjectId(jobId);
+
             // Update the job's applicant count by incrementing it by 1
             await jobsCollection.updateOne(
                 { _id: jobObjectId },
                 { $inc: { jobApplicantsNumber: 1 } }
-              );
-          
+            );
+
             const result = await appliedJobsCollection.insertOne(applicationData);
-          
+
             res.send(result);
-          });
+        });
 
+      // API endpoint for updating a job by ID
 
-          app.delete('/jobs/:id',async(req,res)=>{
+        app.put('/jobs/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('Delete from database:',id);
-            const query = {_id: new ObjectId(id)}
-        
+            const jobs = req.body;
+            console.log('new user', id, jobs);
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedJob = {
+                $set: {
+                    jobBannerURL: jobs.jobBannerURL,
+                    jobTitle: jobs.jobTitle,
+                    loggedInUserName: jobs.loggedInUserName,
+                    jobCategory: jobs.jobCategory,
+                    salaryRange: jobs.salaryRange,
+                    jobDescription: jobs.jobDescription,
+                    jobPostingDate: jobs.jobPostingDate,
+                    applicationDeadline: jobs.applicationDeadline,
+                    jobApplicantsNumber: jobs.jobApplicantsNumber,
+                    userEmail: jobs.userEmail,
+
+                }
+            }
+
+            const result = await jobsCollection.updateOne(filter, updatedJob, options)
+            res.send(result);
+        });
+
+       //API endpoint for deleting a job by ID
+
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('Delete from database:', id);
+            const query = { _id: new ObjectId(id) }
+
             const result = await jobsCollection.deleteOne(query);
             res.send(result);
         })
 
-    
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
