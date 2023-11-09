b3cc30f853857830cc9dff5eaf82f932b4bf4a99
sattamap <sattamap@gmail.com>
2023-11-09 19:03:49 +0600

    update

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const express = require('express');
+require('dotenv').config()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
 
 
 const app = express();
 const port = process.env.PORT || 5000;
+const jwtSecret = process.env.ACCESS_TOKEN_SECRET
 
 
-//middleware
+//middleware 'https://job-nest-94803.web.app',
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173', 'https://job-nest-94803.web.app'
+    ],
+    credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
+
+
+//middlewre
+
+const logger = (req,res, next) =>{
+    console.log('log info:',req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) =>{
+    const token = req?.cookies?.token;
+   console.log('token in the middleware:', token);
+  
+    if(!token){
+        return res.status(401).send({message: "Unauthorized Access"})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+       if(err){
+        return res.status(401).send({message: 'Unauthorized access'})
+       } 
+       console.log("decoded:",decoded);
+       req.user = decoded;
+       next();
+      
+    })
+}
 
 
 
@@ -30,22 +62,29 @@ const client = new MongoClient(uri, {
     }
 });
 
-//middlewre
 
-const logger = (req,res, next) =>{
-    console.log('log info:',req.method, req.url);
-    next();
+
+
+const dbConnect = async () =>{
+    try{
+        client.connect()
+        console.log('DB connected Successfully');
+    }catch(error){
+        console.log(error.name,error.message);
+    }
 }
 
+dbConnect()
 
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7) 
-        // await client.connect();
+
 
         const jobsCollection = client.db('JobNestDB').collection('jobs');
         const appliedJobsCollection = client.db('JobNestDB').collection('appliedJobs');
+
+        app.get('/', (req, res) => {
+            res.send('job-nest-server is running')
+        })
         
         //JWT token generation and cookie setting
 
@@ -72,20 +111,29 @@ async function run() {
         app.post('/logout', async(req,res)=>{
             const user = req.body;
             console.log("log out", user);
-            res.clearCookie('token',{maxAge: 0}).send({success: true})
+            res.clearCookie('token', {
+                maxAge: 0,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+              })
+              .send({ success: true })
         })
 
    
-        //API endpoint for retrieving all jobs
-        app.get('/jobs', async (req, res) => {
-            console.log("Query:",req.query.email);
+        //API endpoint for retrieving all jobs  
+        app.get('/jobs',async (req, res) => {
+        
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
         //API endpoint for retrieving applied jobs
-        app.get('/appliedJobs', async (req, res) => {
-  
+        app.get('/appliedJobs',logger,verifyToken,  async (req, res) => {
+            console.log(req.query.email);
+            console.log('Token owner applied is:',req.user);
+            if(req.user.email !==req.query.email){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
             const cursor = appliedJobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -94,6 +142,8 @@ async function run() {
         // API endpoint for retrieving a single job by ID
 
         app.get('/jobs/:id', async (req, res) => {
+
+        
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
 
@@ -113,7 +163,7 @@ async function run() {
 
         //job applicant count and inserted application data in /appliedJobs API
 
-        app.post('/appliedJobs', async (req, res) => {
+        app.post('/appliedJobs',async (req, res) => {
             const applicationData = req.body;
 
             // Update the job's applicant count using the $inc operator
@@ -174,22 +224,12 @@ async function run() {
 
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        //await client.close();
-    }
-}
-run().catch(console.dir);
 
 
 
 
-app.get('/', (req, res) => {
-    res.send('job-nest-server is running')
-})
+
+
 
 app.listen(port, () => {
     console.log(`job-nest-server is running on post: ${port}`);
